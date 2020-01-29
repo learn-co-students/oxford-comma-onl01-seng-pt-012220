55acd75d75dc9ff01543f80289fd57304b2cad30
Hailey Bongo <haileyebongo@gmail.com>
2020-01-29 04:34:33 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
-def oxford_comma(array)
 
-end
+def oxford_comma(array)
+  oxford = ""
+  if array.size == 2 
+    oxford << "#{array [0]} "
+    oxford << "and "
+    oxford << "#{array[1]}"
+  elsif array.size > 2 
+    j=0
+    until j == array.size-2
+      oxford << "#{array[j]}, "
+      j += 1 
+    end
+    oxford << "#{array[j]}, and "
+    oxford << "#{array.last}"
+    else 
+      oxford << "#{array[0]}"
+    end
+   oxford
+  end
+
+    
+    
