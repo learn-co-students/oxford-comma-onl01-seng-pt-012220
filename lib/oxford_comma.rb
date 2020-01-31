6aa221e1fbb9583d600077e8bea5357b21466e3b
Jordon Terry <jt5493@hotmail.com>
2020-01-31 03:02:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length >= 2
+    array[-1].insert(0, "and ")
+  end
+  if array.length > 2
+    return array.join(", ")
+  else
+    return array.join(" ")
+  end
 end
